eecfab4e6b26f1bbdfaaaa0ebf7d2db29565584b
sivachandranvijayakumar <sivapkm1998@gmail.com>
2023-08-06 20:20:30 +0530

    new

---
+++ main.js
@@ -0,0 +1,49 @@
+//1.display a text
+
+let a="siva";
+console.log(a);
+
+function name(){
+	console.log(a)
+}
+
+name()
+
+
+//2.function with parameters
+
+function hello(b,c){
+	console.log(b,c)
+}
+	hello(10,20)
+
+
+//3.add two numbers
+
+function add(d,e){
+	console.log(d+e)
+}
+add(10,10)
+
+
+//sum of two numbers
+
+function sum(d,e){
+	console.log(d+e)
+}
+sum(10,20)
+
+function sub(d,e){
+	console.log(d-e)
+}
+sub(20,10)
+
+function mul(d,e){
+	console.log(d*e)
+}
+mul(5,5)
+
+function div(d,e){
+	console.log(d/e)
+}
+div(4,4)
